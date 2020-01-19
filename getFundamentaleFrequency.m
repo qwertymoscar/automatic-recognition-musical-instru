0cbf1f1bb4ae91869f09cb28ee545a369b811ac5
qwertymoscar <moussa4.sow@gmail.com>
2020-01-19 14:28:32 +0100

    Find fundamental frequency of a signal

--- getFundamentaleFrequency.m
+++ getFundamentaleFrequency.m
@@ -2,7 +2,8 @@
 %*returns the fondamental frequency and its amplitude*%
 %*****************************************************%
 
-function [fondamental_frequency, amplitude_max] = getFundamentaleFrequency(X,f)
-    [amplitude_max,location_max] = max(abs(X));
+function [fondamental_frequency, amplitude_max, locationMax] = getFundamentaleFrequency(X,f)
+    [amplitude_max, location_max] = max(abs(X));
     fondamental_frequency = abs(f(location_max));
+    locationMax = location_max;
 end
