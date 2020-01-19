af0cbb4f5546d0c202197d536aa47ebb1f70f494
qwertymoscar <moussa4.sow@gmail.com>
2020-01-19 14:29:10 +0100

    Find Harmonics (error occured)

--- get_Harmonic_Features__.m
+++ get_Harmonic_Features__.m
@@ -3,9 +3,13 @@
 %*****************************************************%
 
 function [peak_points] = getHarmonicFeatures(X, f, loc)
+    fo = abs(f(loc));
+    test = fo:fo:3000
     [points, location] = findpeaks(abs(X));
+    aboveMax = mod(f(location),fo) == 0;
+    %aboveMax = points>0.7*0.035138;
     %***********************************%
-    aboveMax = mod(f(location),f(loc)) == 0;
+    %aboveMax = abs(f(location)) == test;
     %***********************************%
     if any(aboveMax)
         peak_point = [points(aboveMax) location(aboveMax)];
